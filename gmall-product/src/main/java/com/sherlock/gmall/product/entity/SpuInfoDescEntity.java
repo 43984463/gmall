d55935414cba8acd951c2119cff7fce5d700c790
sherlock <290274432@qq.com>
2020-06-09 15:28:07 +0800

    Gmall谷粒商城

--- gmall-product/src/main/java/com/sherlock/gmall/product/entity/SpuInfoDescEntity.java
+++ gmall-product/src/main/java/com/sherlock/gmall/product/entity/SpuInfoDescEntity.java
@@ -3,11 +3,10 @@ package com.sherlock.gmall.product.entity;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-
-import java.io.Serializable;
-import java.util.Date;
 import lombok.Data;
 import lombok.experimental.Accessors;
+
+import java.io.Serializable;
 
 /**
  * spu信息介绍
