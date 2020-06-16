bb09366f3d9d888b2739a15b9ee93773b34e3dae
sherlock <290274432@qq.com>
2020-06-16 22:31:55 +0800

    Gmall谷粒商城

--- gmall-search/src/main/java/com/sherlock/gmall/search/config/GmallElasticConfig.java
+++ gmall-search/src/main/java/com/sherlock/gmall/search/config/GmallElasticConfig.java
@@ -1,6 +1,8 @@
 package com.sherlock.gmall.search.config;
 
 import org.apache.http.HttpHost;
+import org.elasticsearch.client.HttpAsyncResponseConsumerFactory;
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.context.annotation.Bean;
@@ -20,5 +22,16 @@ public class GmallElasticConfig {
                 RestClient.builder(new HttpHost("120.76.60.77", 9100, "http"))
         );
         return client;
+    }
+
+
+    public static final RequestOptions COMMON_OPTIONS;
+    static {
+        RequestOptions.Builder builder = RequestOptions.DEFAULT.toBuilder();
+       /* builder.addHeader("Authorization", "Bearer " + TOKEN);
+        builder.setHttpAsyncResponseConsumerFactory(
+                new HttpAsyncResponseConsumerFactory
+                        .HeapBufferedResponseConsumerFactory(30 * 1024 * 1024 * 1024));*/
+        COMMON_OPTIONS = builder.build();
     }
 }
