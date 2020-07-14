b9ebd9b6c6428a24d412fe33d8af0096b2a5e61d
sherlock <290274432@qq.com>
2020-07-14 14:14:40 +0800

    Gmall谷粒商城

--- gmall-product/src/main/java/com/sherlock/gmall/product/web/IndexController.java
+++ gmall-product/src/main/java/com/sherlock/gmall/product/web/IndexController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Author xueshuai
@@ -47,15 +48,33 @@ public class IndexController {
     @ResponseBody
     @GetMapping("/hello")
     public String hello() {
+        // 1、获取一把分布式锁，只要锁的名称一样，那就是同一把锁
         RLock mylock = redisson.getLock("mylock");
 
+        /**
+         *
+         * 2、加锁
+         *  redisson分布式锁特点
+         * mylock.lock(); //阻塞式等待，默认加的锁都是30S过期。
+         * 锁会自动续期，如果业务时间超长，运行期间自动给锁续期到30S，不用担心业务时间长锁被自动删掉
+         * 当业务运行完成，就不会再自动续期，即使不手动解锁，锁也会在30S之后自动删除
+         *
+         * mylock.lock(20, TimeUnit.SECONDS);
+         * 在获取锁之后会在20S之后自动解锁，所以要慎用，尽量不用
+         * 假如业务时间超过20S，业务执行完之后再去解锁，解锁的就不是当前线程的分布式锁，会报错
+         */
+
         mylock.lock(); // 加锁 阻塞式等待
+        // mylock.lock(20, TimeUnit.SECONDS);
+        // mylock.tryLock();
+        mylock.lock();
         try {
             System.out.println(Thread.currentThread().getId() + "---> 加锁成功");
             Thread.sleep(10000);
         } catch (Exception ex) {
             ex.printStackTrace();
         } finally {
+            // 3、手动解锁 即使解锁代码没有运行，redisson也不会出现死锁问题
             mylock.unlock(); // 解锁
             System.out.println(Thread.currentThread().getId() + " ---> 释放锁");
         }
