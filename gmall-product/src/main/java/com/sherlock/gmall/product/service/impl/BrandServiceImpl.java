42b3484c2d693f4ed3194ab51260eff18d9d8270
sherlock <290274432@qq.com>
2020-08-06 14:56:23 +0800

    Gmall谷粒商城

--- gmall-product/src/main/java/com/sherlock/gmall/product/service/impl/BrandServiceImpl.java
+++ gmall-product/src/main/java/com/sherlock/gmall/product/service/impl/BrandServiceImpl.java
@@ -1,25 +1,22 @@
 package com.sherlock.gmall.product.service.impl;
 
-import com.sherlock.gmall.product.service.AttrAttrgroupRelationService;
-import com.sherlock.gmall.product.service.CategoryBrandRelationService;
-import org.apache.commons.lang.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.sherlock.common.utils.PageUtils;
 import com.sherlock.common.utils.Query;
-
 import com.sherlock.gmall.product.dao.BrandDao;
 import com.sherlock.gmall.product.entity.BrandEntity;
 import com.sherlock.gmall.product.service.BrandService;
+import com.sherlock.gmall.product.service.CategoryBrandRelationService;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 @Service("brandService")
