c0f326606d49de4e2f9ad4d335d0f0e70c62b07c
sherlock <290274432@qq.com>
2020-08-19 12:45:35 +0800

    Gmall谷粒商城

--- gmall-auth-server/src/main/java/com/sherlock/gmall/auth/config/GmallWebConfig.java
+++ gmall-auth-server/src/main/java/com/sherlock/gmall/auth/config/GmallWebConfig.java
@@ -30,12 +30,12 @@ public class GmallWebConfig implements WebMvcConfigurer {
 
     }
 
-    @Override
+    /*@Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowCredentials(true)
                 .allowedHeaders("*")
                 .allowedOrigins("*")
                 .allowedMethods("*");
-    }
+    }*/
 }
