4028e1a8d000d6ad9d5c103c279a81a5f72dd6b2
Sherlock <+XSxs100206@>
2020-08-27 22:23:36 +0800

    gmall谷粒商城  单点登录sso

---
+++ gmall-sso-client1/src/main/java/com/sherlock/gmall/ssoclient/GmallSsoClient1Application.java
@@ -0,0 +1,13 @@
+package com.sherlock.gmall.ssoclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmallSsoClient1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmallSsoClient1Application.class, args);
+    }
+
+}
---
+++ gmall-sso-server/src/test/java/com/sherlock/gmall/ssoserver/GmallTestSsoServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sherlock.gmall.ssoserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GmallTestSsoServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
