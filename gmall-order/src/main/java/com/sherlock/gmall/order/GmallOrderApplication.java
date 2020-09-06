8cf9ac14ffa5f9ecaf90f5dba4554a7d49584b20
sherlock <290274432@qq.com>
2020-09-06 17:55:35 +0800

    Gmall谷粒商城  seata使用步骤

--- gmall-order/src/main/java/com/sherlock/gmall/order/GmallOrderApplication.java
+++ gmall-order/src/main/java/com/sherlock/gmall/order/GmallOrderApplication.java
@@ -25,6 +25,51 @@ package com.sherlock.gmall.order;
  *  AmqpAdmin 组件可以用来创建交换机，队列，绑定关系等
  *  RabbitTemplate 可以用来发送消息
  *  @RabbitListener 可以用来监听接收消息
+ *
+ *
+ *  本地事务失效问题：
+ *  同一个对象内事务方法互调默认失效，原因 绕过了代理对象，事务使用代理对象来控制的
+ *
+ *  解决： 使用代理对象来调用事务方法
+ *  1）、引入
+ *          <dependency>
+ *             <groupId>org.springframework.boot</groupId>
+ *             <artifactId>spring-boot-starter-aop</artifactId>
+ *         </dependency>
+*       这个包含了 aspectjweaver
+ *  2）、@EnableAspectJAutoProxy(exposeProxy = true)  开启aspectj动态代理功能，以后所有的动态代理都是aspectj创建的(即使没有接口也可以创建动态代理)
+ *  3）、 用调用对象本类互调
+ *  Object o = AopContext.currentProxy(); 再强制转换为本类 然后进行调用
+ *
+ *
+ *
+ *
+ *  seata控制分布式事务
+ *  参考 http://seata.io/zh-cn/docs/user/quickstart.html
+ *  1）、没个微服务先必须创建undo_log表，
+ *
+ *    drop table if exists undo_log;
+ *
+ *   CREATE TABLE `undo_log` (
+ *       `id` bigint(20) NOT NULL AUTO_INCREMENT,
+ *       `branch_id` bigint(20) NOT NULL,
+ *       `xid` varchar(100) NOT NULL,
+ *      `context` varchar(128) NOT NULL,
+ *      `rollback_info` longblob NOT NULL,
+ *      `log_status` int(11) NOT NULL,
+ *      `log_created` datetime NOT NULL,
+ *      `log_modified` datetime NOT NULL,
+ *      `ext` varchar(100) DEFAULT NULL,
+ *       PRIMARY KEY (`id`),
+ *      UNIQUE KEY `ux_undo_log` (`xid`,`branch_id`)
+ *   ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+ *
+ *   alter table undo_log comment 'seata日志表';
+ *
+ *   2）、安装事务协调器
+ *   从 https://github.com/seata/seata/releases ,下载服务器软件包，将其解压缩。
+ *
+ *
  */
 
 import com.sherlock.common.constants.GmallConstant;
@@ -32,7 +77,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+@EnableAspectJAutoProxy(exposeProxy = true)
 @EnableFeignClients(GmallConstant.GMALL_ORDER_BASEPATH + GmallConstant.FEIGN)
 @EnableDiscoveryClient
 @SpringBootApplication
---
+++ gmall-order/src/main/java/com/sherlock/gmall/order/dao/main.java
@@ -0,0 +1,49 @@
+package com.sherlock.gmall.order.dao;
+
+import com.sherlock.gmall.order.entity.OrderEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * @Author xueshuai
+ * @Description: TODO
+ * @Date 2020/9/6
+ **/
+public class main {
+    public static void main(String[] args) {
+        List<Test> list = new ArrayList<>();
+        Test test1 = new Test("1", new BigDecimal(20.54));
+        Test test2 = new Test("2", new BigDecimal(21));
+        Test test3 = new Test("1", new BigDecimal(23));
+        Test test4 = new Test("3", new BigDecimal(24));
+        Test test5 = new Test("4", new BigDecimal(25));
+        Test test6 = new Test("5", new BigDecimal(26));
+        Test test7 = new Test("6", new BigDecimal(27));
+        Test test8 = new Test("3", new BigDecimal(28));
+        list.add(test1);
+        list.add(test2);
+        list.add(test3);
+        list.add(test4);
+        list.add(test5);
+        list.add(test6);
+        list.add(test7);
+        list.add(test8);
+        Map<String, Double> collect = list.stream().collect(Collectors.groupingBy(Test::getId, Collectors.summingDouble(test -> test.getValue().doubleValue())));
+        System.out.println(collect);
+    }
+}
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+class Test{
+    private String id;
+    private BigDecimal value;
+}
