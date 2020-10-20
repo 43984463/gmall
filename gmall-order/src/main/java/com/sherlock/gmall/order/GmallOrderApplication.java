249e7868ff2713ce89a71e027f068710fa8bce4b
sherlock <290274432@qq.com>
2020-10-20 18:32:50 +0800

    Gmall谷粒商城  Spring扩展接口

--- gmall-order/src/main/java/com/sherlock/gmall/order/GmallOrderApplication.java
+++ gmall-order/src/main/java/com/sherlock/gmall/order/GmallOrderApplication.java
@@ -85,6 +85,161 @@ package com.sherlock.gmall.order;
  *   5）、给分布式事务大事务的入口标注 @GlobalTransactional
  *   6）、给每个远程的小事务用spring的@Transactional即可
  *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *   SpringBoot初始化过程中的扩展点
+ *   （一）容器初始化之前
+ *
+ *      接口方法：
+ *      @see org.springframework.context.ApplicationContextInitializer#initialize(org.springframework.context.ConfigurableApplicationContext)
+ *
+ *      这是整个spring容器在刷新之前初始化ConfigurableApplicationContext的回调接口，
+ *      简单来说，就是在容器刷新之前调用此类的initialize方法。这个点允许被用户自己扩展。用户可以在整个spring容器还没被初始化之前做一些事情。
+ *      可以想到的场景可能为，在最开始激活一些配置，或者利用这时候class还没被类加载器加载的时机，进行动态字节码注入等操作。
+ *
+ *
+ *      调用点：
+ *      @see org.springframework.boot.SpringApplication#run(java.lang.String...) -->
+ *           @see org.springframework.boot.SpringApplication#prepareContext(org.springframework.context.ConfigurableApplicationContext, org.springframework.core.env.ConfigurableEnvironment, org.springframework.boot.SpringApplicationRunListeners, org.springframework.boot.ApplicationArguments, org.springframework.boot.Banner) -->
+ *               @see org.springframework.boot.SpringApplication#applyInitializers(org.springframework.context.ConfigurableApplicationContext)
+ *               initializer.initialize(context);
+ *
+ *      因为这时候spring容器还没被初始化，所以想要自己的扩展的生效，有以下三种方式：
+ *      公共部分： 重写 {@link org.springframework.context.ApplicationContextInitializer#initialize(org.springframework.context.ConfigurableApplicationContext)}
+ *           public class TestApplicationContextInitializer implements ApplicationContextInitializer {
+ *              @Override
+ *              public void initialize(ConfigurableApplicationContext applicationContext) {
+ *                  System.out.println("[ApplicationContextInitializer]");
+ *              }
+ *          }
+ *     添加方式一：
+ *          在启动类中用springApplication.addInitializers(new TestApplicationContextInitializer())语句加入
+ *           @see org.springframework.boot.SpringApplication#addInitializers(org.springframework.context.ApplicationContextInitializer[])
+ *
+ *           例子：
+ *
+ *          public class GmallOrderApplication {
+ *
+ *               public static void main(String[] args) {
+ *                   SpringApplication springApplication = new SpringApplication(GmallOrderApplication.class);
+ *                   springApplication.addInitializers(new TestApplicationContextInitializer());
+ *                   springApplication.run(args);
+ *
+ *                  // 分解 SpringApplication.run(GmallOrderApplication.class, args);
+ *               }
+ *
+ *          }
+ *
+ *
+ *      添加方式二：
+ *        配置文件配置context.initializer.classes=com.example.demo.TestApplicationContextInitializer
+ *      添加方式三：
+ *        Spring SPI扩展，在spring.factories中加入
+ *        org.springframework.context.ApplicationContextInitializer=com.example.demo.TestApplicationContextInitializer
+ *
+ *        @see org.springframework.core.io.support.SpringFactoriesLoader#FACTORIES_RESOURCE_LOCATION
+ *        加载位置：从springboot主启动类开始
+ *        @see org.springframework.boot.SpringApplication#SpringApplication(org.springframework.core.io.ResourceLoader, java.lang.Class[])  --> setInitializers((Collection) getSpringFactoriesInstances(ApplicationContextInitializer.class)); -->
+ *              @see org.springframework.boot.SpringApplication#getSpringFactoriesInstances(java.lang.Class) -->
+ *                   @see org.springframework.boot.SpringApplication#getSpringFactoriesInstances(java.lang.Class, java.lang.Class[], java.lang.Object...) --> Set<String> names = new LinkedHashSet<>(SpringFactoriesLoader.loadFactoryNames(type, classLoader)); -->
+ *                       @see org.springframework.core.io.support.SpringFactoriesLoader#loadFactoryNames(java.lang.Class, java.lang.ClassLoader) -->
+ *                           @see org.springframework.core.io.support.SpringFactoriesLoader#loadSpringFactories(java.lang.ClassLoader)
+ *                           在此处加载 {@link org.springframework.core.io.support.SpringFactoriesLoader#FACTORIES_RESOURCE_LOCATION}文件中的所有类
+ *
+ *
+ *
+ *
+ *
+ *    （二）
+ *         接口方法：
+ *          @see org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor#postProcessBeanDefinitionRegistry(org.springframework.beans.factory.support.BeanDefinitionRegistry)
+ *          容器初始化的时候添加bean定义信息beanDefinition
+ *          这个接口在读取项目中的beanDefinition之后执行，提供一个补充的扩展点
+ *          使用场景：你可以在这里动态注册自己的beanDefinition，可以加载classpath之外的bean
+ *         调用点：
+ *         @see org.springframework.boot.SpringApplication#run(java.lang.String...) -->
+ *              @see org.springframework.boot.SpringApplication#refreshContext(org.springframework.context.ConfigurableApplicationContext)
+ *                   @see org.springframework.boot.SpringApplication#refresh(org.springframework.context.ApplicationContext)
+ *                        @see org.springframework.context.support.AbstractApplicationContext#refresh()  --> invokeBeanFactoryPostProcessors(beanFactory);
+ *                              @see org.springframework.context.support.AbstractApplicationContext#invokeBeanFactoryPostProcessors(org.springframework.beans.factory.config.ConfigurableListableBeanFactory) -->
+ *                                   @see org.springframework.context.support.PostProcessorRegistrationDelegate#invokeBeanFactoryPostProcessors(org.springframework.beans.factory.config.ConfigurableListableBeanFactory, java.util.List)
+ *                                   2处使用
+ *                                   ①
+ *                                   registryProcessor.postProcessBeanDefinitionRegistry(registry);
+ *                                   ②
+ *                                   @see org.springframework.context.support.PostProcessorRegistrationDelegate#invokeBeanDefinitionRegistryPostProcessors(java.util.Collection, org.springframework.beans.factory.support.BeanDefinitionRegistry)
+ *
+ *          添加方式:
+ *           @Component
+ *          public class MyBeanDefinitionRegistryPostProcessor implements BeanDefinitionRegistryPostProcessor{
+ *
+ *          	public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+ *          		System.out.println("MyBeanDefinitionRegistryPostProcessor...bean的数量："+beanFactory.getBeanDefinitionCount());
+ *          	}
+ *
+ *          	//BeanDefinitionRegistry Bean定义信息的保存中心，以后BeanFactory就是按照BeanDefinitionRegistry里面保存的每一个bean定义信息创建bean实例；
+ *          	public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry) throws BeansException {
+ *          		System.out.println("postProcessBeanDefinitionRegistry...bean的数量："+registry.getBeanDefinitionCount());
+ *          		//RootBeanDefinition beanDefinition = new RootBeanDefinition(Blue.class);
+ *          	    // 添加一个Blue到容器中作为组件
+ *          		AbstractBeanDefinition beanDefinition = BeanDefinitionBuilder.rootBeanDefinition(Blue.class).getBeanDefinition();
+ *          		registry.registerBeanDefinition("hello", beanDefinition);
+ *          	}
+ *
+ *          }
+ *      (三)、
+ *           接口方法：
+ *           @see org.springframework.beans.factory.config.BeanFactoryPostProcessor#postProcessBeanFactory(org.springframework.beans.factory.config.ConfigurableListableBeanFactory)
+ *              容器初始化的时候在spring读取beanDefinition信息之后，实例化bean之前。
+ *              这个接口是beanFactory的扩展接口，调用时机在spring在读取beanDefinition信息之后，实例化bean之前。
+ *              在这个时机，用户可以通过实现这个扩展接口来自行处理一些东西，比如修改已经注册的beanDefinition的元信息。
+ *           调用点：
+ *           @see org.springframework.boot.SpringApplication#run(java.lang.String...) -->
+ *                @see org.springframework.boot.SpringApplication#refreshContext(org.springframework.context.ConfigurableApplicationContext)
+ *                     @see org.springframework.boot.SpringApplication#refresh(org.springframework.context.ApplicationContext)
+ *                          @see org.springframework.context.support.AbstractApplicationContext#refresh()  --> invokeBeanFactoryPostProcessors(beanFactory);
+ *                                @see org.springframework.context.support.AbstractApplicationContext#invokeBeanFactoryPostProcessors(org.springframework.beans.factory.config.ConfigurableListableBeanFactory) -->
+ *                                     @see org.springframework.context.support.PostProcessorRegistrationDelegate#invokeBeanFactoryPostProcessors(org.springframework.beans.factory.config.ConfigurableListableBeanFactory, java.util.List)
+ *                                          @see org.springframework.context.support.PostProcessorRegistrationDelegate#invokeBeanFactoryPostProcessors(java.util.Collection, org.springframework.beans.factory.config.ConfigurableListableBeanFactory)
+ *
+ *                                          postProcessor.postProcessBeanFactory(beanFactory);
+ *
+ *      (四)、
+ *          接口方法：
+ *          @see org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessor extends BeanPostProcessor
+ *          该接口继承了 BeanPostProcessor 接口，区别如下:
+ *             ** postProcessBeforeInstantiation：实例化bean之前，相当于new这个bean之前
+ *             ** postProcessAfterInstantiation：实例化bean之后，相当于new这个bean之后
+ *             ** postProcessPropertyValues：bean已经实例化完成，在属性注入时阶段触发，@Autowired,@Resource等注解原理基于此方法实现
+ *             ** postProcessBeforeInitialization：初始化bean之前，相当于把bean注入spring上下文之前
+ *             ** postProcessAfterInitialization：初始化bean之后，相当于把bean注入spring上下文之后
+ *
+ *          使用场景：这个扩展点非常有用 ，无论是写中间件和业务中，都能利用这个特性。比如对实现了某一类接口的bean在各个生命期间进行收集，或者对某个类型的bean进行统一的设值等等。
+ *
+ *          1、实例化----实例化的过程是一个创建Bean的过程，即调用Bean的构造函数，单例的Bean放入单例池中   -> InstantiationAwareBeanPostProcessor的接口起作用
+ *
+ *          2、初始化----初始化的过程是一个赋值的过程，即调用Bean的setter，设置Bean的属性 -> BeanPostProcessor的接口起作用
+ *
+ *          @see org.springframework.beans.factory.support.AbstractAutowireCapableBeanFactory#createBean(java.lang.String, org.springframework.beans.factory.support.RootBeanDefinition, java.lang.Object[])
+ *          //  Give BeanPostProcessors a chance to return a proxy instead of the target bean instance.
+ * 			    Object bean = resolveBeforeInstantiation(beanName, mbdToUse);
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
  */
 
 import com.sherlock.common.constants.GmallConstant;
