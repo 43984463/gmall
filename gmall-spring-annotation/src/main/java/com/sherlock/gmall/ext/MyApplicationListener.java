ae36f0f85f6bb0138320bd3725625a9560560687
Sherlock <+XSxs100206@>
2020-10-25 22:17:05 +0800

    gmall谷粒商城   Spring Annotation

---
+++ gmall-spring-annotation/src/main/java/com/sherlock/gmall/ext/MyApplicationListener.java
@@ -0,0 +1,21 @@
+package com.sherlock.gmall.ext;
+
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.stereotype.Component;
+
+@EnableAsync
+@Component
+@Async
+public class MyApplicationListener implements ApplicationListener<ApplicationEvent> {
+
+	//当容器中发布此事件以后，方法触发
+	@Override
+	public void onApplicationEvent(ApplicationEvent event) {
+		// TODO Auto-generated method stub
+		System.out.println("收到事件："+event);
+	}
+
+}
