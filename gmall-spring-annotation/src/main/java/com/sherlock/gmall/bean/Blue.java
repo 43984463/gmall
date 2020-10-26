17fa4df69bb6e295cb6e859570e1e5d9758480af
sherlock <290274432@qq.com>
2020-10-26 16:06:35 +0800

    Gmall谷粒商城  Spring Annotation

--- gmall-spring-annotation/src/main/java/com/sherlock/gmall/bean/Blue.java
+++ gmall-spring-annotation/src/main/java/com/sherlock/gmall/bean/Blue.java
@@ -1,9 +1,23 @@
 package com.sherlock.gmall.bean;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
+
 /**
  * @Author xueshuai
  * @Description: TODO
  * @Date 2020/10/23
  **/
-public class Blue {
+public class Blue implements BeanDefinitionRegistryPostProcessor {
+    @Override
+    public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry) throws BeansException {
+        System.out.println("Blue===========================================postProcessBeanDefinitionRegistry");
+    }
+
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        System.out.println("Blue===========================================postProcessBeanFactory");
+    }
 }
