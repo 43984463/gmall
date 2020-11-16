35854585074883d36ee2e1a7efe22ae210099a6e
Sherlock <+XSxs100206@>
2020-11-16 16:15:21 +0800

    gmall谷粒商城   常见面试题

--- gmall-order/src/main/java/com/sherlock/gmall/order/GmallOrderApplication.java
+++ gmall-order/src/main/java/com/sherlock/gmall/order/GmallOrderApplication.java
@@ -36,7 +36,7 @@ package com.sherlock.gmall.order;
  *             <groupId>org.springframework.boot</groupId>
  *             <artifactId>spring-boot-starter-aop</artifactId>
  *         </dependency>
-*       这个包含了 aspectjweaver
+*       这个包含了 aspectjWeaver
  *  2）、@EnableAspectJAutoProxy(exposeProxy = true)  开启aspectj动态代理功能，以后所有的动态代理都是aspectj创建的(即使没有接口也可以创建动态代理)
  *  3）、 用调用对象本类互调
  *  Object o = AopContext.currentProxy(); 再强制转换为本类 然后进行调用
@@ -46,7 +46,7 @@ package com.sherlock.gmall.order;
  *
  *  seata控制分布式事务
  *  参考 http://seata.io/zh-cn/docs/user/quickstart.html
- *  1）、没个微服务先必须创建undo_log表，
+ *  1）、每个微服务先必须创建undo_log表，
  *
  *    drop table if exists undo_log;
  *
